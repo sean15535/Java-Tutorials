0796170efeda8de084c15b88f0e4a6ae25115864
sean15535 <sean15535@gmail.com>
2025-06-22 17:44:27 +0100

    Add initial Java project files and configuration

---
+++ exercise/HelloWorld.java
@@ -0,0 +1,8 @@
+package exercise;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
