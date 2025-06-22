c09292acbb7e8e734654c246004b8f1ea37364a8
sean15535 <sean15535@gmail.com>
2025-06-22 18:03:43 +0100

    Add additional output line to HelloWorld.java

--- exercise/HelloWorld.java
+++ exercise/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         // This line prints "Hello, World!" to the console.
         System.out.println("Hello, World!");
         System.out.println("I love Java");
+        System.out.println(21+45);
     }
 }
