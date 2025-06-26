7440aec5b400a1f252c2837d3b7654fec18fd0c8
sean15535 <sean15535@gmail.com>
2025-06-26 20:55:23 +0100

    Refactor HelloWorld.java to include descriptive comments and add QuizGame.class file

--- exercise/HelloWorld.java
+++ exercise/HelloWorld.java
@@ -1,5 +1,5 @@
-package exercise;
-
+// This is a simple Java program that prints "Hello, World!" to the console.
+// It also prints an additional message and the result of a mathematical operation.
 public class HelloWorld {
     public static void main(String[] args) {
         // This line prints "Hello, World!" to the console.
