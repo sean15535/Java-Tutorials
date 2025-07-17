a20e14a744277560aa5aa20a78a865b0c81db83f
sean15535 <sean15535@gmail.com>
2025-07-17 23:32:25 +0100

    Add main method to Book class for testing functionality

--- exercise/Book.java
+++ exercise/Book.java
@@ -32,4 +32,15 @@ public class Book {
     public String getAuthor() {
         return author;
     }
+
+    public static void main(String[] args) {
+        Book book = new Book("The Hobbit", "J.R.R. Tolkien");
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthor());
+        System.out.println("Available: " + book.isAvailable());
+        book.checkOut();
+        System.out.println("Available after checkout: " + book.isAvailable());
+        book.returnBook();
+        System.out.println("Available after return: " + book.isAvailable());
+    }
 }
