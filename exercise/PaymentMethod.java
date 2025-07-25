b6c5736141e6d6d78d2192286183a3cca72a80d3
sean15535 <sean15535@gmail.com>
2025-07-25 02:48:50 +0100

    Refactor PaymentMethod and its subclasses to enhance payment processing implementation

--- exercise/PaymentMethod.java
+++ exercise/PaymentMethod.java
@@ -1,12 +1,33 @@
+// PaymentMethod.java
 public class PaymentMethod {
     public void processPayment(double amount) {
-        // Generic implementation
+        System.out.println("Processing generic payment of $" + amount);
     }
 }
 
+// CreditCardPayment.java
 public class CreditCardPayment extends PaymentMethod {
     @Override
     public void processPayment(double amount) {
-        // Credit card-specific implementation
+        System.out.println("Processing credit card payment of $" + amount);
+    }
+}
+
+// PayPalPayment.java
+public class PayPalPayment extends PaymentMethod {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount);
+    }
+}
+
+// PaymentExample.java
+public class PaymentExample {
+    public static void main(String[] args) {
+        PaymentMethod payment1 = new CreditCardPayment();
+        PaymentMethod payment2 = new PayPalPayment();
+
+        payment1.processPayment(100.0);
+        payment2.processPayment(200.0);
     }
 }
